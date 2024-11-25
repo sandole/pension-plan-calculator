fd152c804ef7044daf515a5ac16428eb19a8d0f0
John Seong <john.seong@clio.com>
2024-11-24 19:56:06 -0800

    add optional NEXTAUTH_URL to environment variables

--- src/env.js
+++ src/env.js
@@ -17,6 +17,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    NEXTAUTH_URL: z.string().url().optional(),
   },
 
   /**
@@ -38,6 +39,7 @@ export const env = createEnv({
     AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
